a4098b6e7d5b26165b065844d682a63216775eb0
tchaiman <tchaiman@udesa.edu.ar>
2022-11-16 14:43:21 -0300

    casi las series

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 let api_key = '81faef6942a31915ed87b416fbba64ba'
 let url_popu  = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
 let url_latest  = `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`;
+let url_series = `https://api.themoviedb.org/3/tv/top_rated?api_key=${api_key}&language=en-US&page=1`;
 let listaPeli = document.querySelector('.cont-listas_pelis');
 let listaLatest = document.querySelector('.cont-listas_latest');
+let listaSeries = document.querySelector('.cont-listas_series')
 fetch(url_popu)
 .then(function(respuesta) {
     return respuesta.json();
@@ -48,6 +50,31 @@ fetch(url_latest)
                                         <h3>${latest.title}</h3>
                                         <p>${latest.release_date}</p>
                                         <a href="./detail-movie.html?idPelicula=${latest.id}">Ver mas</a>
+                                    </article>` 
+    }
+
+
+
+    return data;
+}).catch(function(error) {
+    console.log(error);
+    return error;
+});
+
+fetch(url_series)
+.then(function(respuesta) {
+    return respuesta.json();
+}).then(function(data) {
+
+    console.log(data.results);
+    for (let i = 0; i < 5; i++) {
+        
+        let series = data.results[i];
+        listaSeries.innerHTML += `<article class="item">
+        <a href="./detail-movie.html"> <img class="imagenes" src="https://image.tmdb.org/t/p/w500/${series.poster_path}" alt=""></a>
+                                        <h3>${series.title}</h3>
+                                        <p>${series.release_date}</p>
+                                        <a href="./detail-movie.html?idPelicula=${series.id}">Ver mas</a>
                                     </article>` 
     }
 
