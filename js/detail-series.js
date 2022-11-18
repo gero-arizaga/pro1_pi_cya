a2cddb8c5bc1df53e704847d8a64010f01c616fb
isayukelson <iyukelson@udesa.edu.ar>
2022-11-18 15:09:32 -0300

    details mov

--- js/detail-series.js
+++ js/detail-series.js
@@ -1,11 +1,11 @@
-let algo = new URLSearchParams(this.location.search)
+let alg = new URLSearchParams(this.location.search)
 let tv_id = algo.get("q")
 console.log(tv_id)
 
 const api_key = '81faef6942a31915ed87b416fbba64ba'
 
-let url = `https://api.themoviedb.org/3/tv/top_rated?${tv_id}?api_key=${api_key}&language=en-US&page=1`
-//let url =`https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}&language=en-US`
+//let url = `https://api.themoviedb.org/3/tv/top_rated?${tv_id}?api_key=${api_key}&language=en-US&page=1`
+let url = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}&language=en-US&page=1`
 
 fetch(url)
     .then(function (respuesta) {
@@ -18,14 +18,15 @@ fetch(url)
         let genero_series = document.querySelector('.genero_serie');
         let descripcion_serie = document.querySelector('.descripcion_serie');
         let imagen = document.querySelector('.item-detail');
-
-        subtitulos.innerText += `${data.name}`
-        fecha_estreno.innerText += `${data.first_air_date}`
-        genero_series.innerText += `${data.genres[0].name}`
-        descripcion_serie.innerText += `${data.overview}`
-        imagen.innerHTML += `<img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="">`
+        
+        subtitulos.innerText += `${ data.title }`
+        fecha_estreno.innerText += `${ data.release_date } `
+        genero_series.innerText += `${ data.genres[0].name } `
+        descripcion_serie.innerText += `${ data.overview } `
+        imagen.innerHTML += `< img src = "https://image.tmdb.org/t/p/w500/${data.poster_path}" alt = "" > `
         
     })
     .catch(function (error) {
         console.log(error);
     })
+
