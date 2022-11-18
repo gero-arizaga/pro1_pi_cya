da3afac969a4e6a99c84d1232daf5e061a420021
gero-arizaga <garizaga@udesa>
2022-11-18 17:08:37 -0300

    botonfv

--- js/detail-series.js
+++ js/detail-series.js
@@ -1,11 +1,43 @@
+//BOTÓN PARA FAVORITOS
+let qs = location.search;
+let qsObj = new URLSearchParams(qs);
+let idserie = qsObj.get ('idserie');
+let id = idserie;
+let boton = document.querySelector ('.boton-fav');
+let favoritos = [];
+let recuperoStorage = localStorage.getItem('favoritos');
+/* Preguntamos si es distinto de nulo-  es verdarero quiero covertirlo de JSON a un array */
+if(recuperoStorage != null){
+    favoritos = JSON.parse(recuperoStorage);
+
+};
+if (favoritos.includes(idserie)) {
+    boton.innerText="Quitar de Favorito";
+}
+boton.addEventListener("click",function (e) {
+    e.preventDefault()
+    if(favoritos.includes(id)){
+        let indice = favoritos.indexOf(id);
+        favoritos.splice(indice,1);
+        boton.innerText="Agregar a Favorito";
+    }else{
+        favoritos.push(id);
+        boton.innerText="Quitar de Favorito";
+    }
+    let favToString = JSON.stringify(favoritos);
+    localStorage.setItem('favoritos',favToString)
+    
+});
+
+
 let alg = new URLSearchParams(this.location.search)
 let tv_id = algo.get("q")
 console.log(tv_id)
 
 const api_key = '81faef6942a31915ed87b416fbba64ba'
 
-//let url = `https://api.themoviedb.org/3/tv/top_rated?${tv_id}?api_key=${api_key}&language=en-US&page=1`
-let url = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}&language=en-US&page=1`
+let url = `https://api.themoviedb.org/3/tv/top_rated?${tv_id}?api_key=${api_key}&language=en-US&page=1`
+//let url = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}&language=en-US&page=1`
 
 fetch(url)
     .then(function (respuesta) {
--- js/favorite.js
+++ js/favorite.js
@@ -1,10 +1,10 @@
+let api_key = '81faef6942a31915ed87b416fbba64ba'
 let recuperoStorage = localStorage.getItem('favoritos');
-
 let favoritos = JSON.parse(recuperoStorage);
 /*  [2,4,6] */
 
 let section = document.querySelector("#lista");
-let personajesFavoritos = ''; /* ---------------------->   OJALDRE ACA */
+let peliculasFavoritas = ''; /* ---------------------->   OJALDRE ACA */
 
 console.log(favoritos);
 
@@ -12,11 +12,11 @@ console.log(favoritos);
 
 if (favoritos == null || favoritos.length == 0) {
     /* Muestres no hay favoritos */
-    section.innerHTML = '<p>No hay personajes en favoritos</p>'
+    section.innerHTML = '<p>No hay peliculas en favoritos</p>'
 } else {
     
     for (let i = 0; i < favoritos.length; i++) {
-        let url =   `https://rickandmortyapi.com/api/character/${favoritos[i]}`
+        let url =   `https://api.themoviedb.org/3/tv/top_rated?${favoritos[i]}`
 
         fetch(url)
         .then(function (response) {
@@ -28,7 +28,7 @@ if (favoritos == null || favoritos.length == 0) {
                                         <p>Name: <a href="./detallePersonaje.html?idPersonaje=${data.id}"> ${data.name}</a> </p>
                                         <p>Status: ${data.status} </p>
                                     </article>`;
-            section.innerHTML = personajesFavoritos;
+            section.innerHTML = pelicualsFavoritas;
 
             return data;
         }).catch(function (error) {
