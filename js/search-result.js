128fa03a522eca702b833e9195fce1eff7a70c37
gero-arizaga <garizaga@udesa>
2022-11-18 13:51:26 -0300

    S_results_mejor

--- js/search-result.js
+++ js/search-result.js
@@ -1,30 +1,32 @@
 let api_key = '81faef6942a31915ed87b416fbba64ba'
 
 // barra de busqueda 
-let searchBaseUrl = ""
+let urlbusqueda = ""
 
 let searchBar = document.getElementById("searchBar");
 function runSearch(keyword) {
-  let url = "".$(searchBaseUrl, api_key, "&query=", keyword)
-  document.getElementById("results-list").innerHTML = "";
-  fetch(url)
+    let url = `${urlbusqueda}${api_key}&query=${keyword}`;
+    document.getElementById("results-list").innerHTML = "";
+    fetch(url)
     .then(result => result.json())
     .then(function (data) {
-      let seriesSearchResult = data.results;
-        if (data.results.length !== 0){
-          let resultsList = document.getElementById("results-list");
-          for (let i = 0; i < seriesSearchResult.length; i++) {
-            id = seriesSearchResult[i].id
-            if (seriesSearchResult[i].poster_path !== null) {
+    let seriesSearchResult = data.results;
+    if (data.results.length !== 0){
+        let resultsList = document.getElementById("resultado-busqueda");
+        for (let i = 0; i < seriesSearchResult.length; i++) {
+        id = seriesSearchResult[i].id
+        if (seriesSearchResult[i].poster_path !== null) {
             resultsList.innerHTML += "<ul class='result'><li>" + seriesSearchResult[i].name + "</li>" + "<li><a id='detalles' onclick='serieSelected("+ id +")' href='page5-detalle-series.html'><img class='series-img' src='https://image.tmdb.org/t/p/original/" + seriesSearchResult[i].poster_path + "' alt=''></a></li></ul>"
-          } else {
+            } 
+        else {
             resultsList.innerHTML += "<ul class='result'><li>" + seriesSearchResult[i].name + "</li>" + "<li><a id='detalles' onclick='serieSelected("+ id +")' href='page5-detalle-series.html'><img class='series-img' src='assets/IMG/noimage.png' alt=''></a></li></ul>"
-          }
+            }
         }
-      } else {
+    } 
+    else {
         let resultTitle = document.getElementById("results-title")
         resultTitle.innerHTML = "No search results found";
-      }
+    }
     
     })
 }
