ba3dd1a6233ab5ec97a3712a0a04064b77e6ff98
gero-arizaga <garizaga@udesa>
2022-11-22 14:53:33 -0300

    E

--- js/favorite.js
+++ js/favorite.js
@@ -7,7 +7,7 @@ let recuperoStorage = localStorage.getItem('favoritos_p');
 let favoritos_p = JSON.parse(recuperoStorage);
 /*  [2,4,6] */
 
-let section = document.querySelector("#lista_p");
+let section = document.querySelector('#lista_p');
 let Peliculas_Favoritas = ''; /* ---------------------->   OJALDRE ACA */
 
 console.log(favoritos_p);
@@ -27,18 +27,21 @@ if (favoritos_p == null || favoritos_p.length == 0) {
         fetch(url)
         .then(function (response) {
             return response.json();
-        }).then(function (data) {
+        })
+        .then(function (data) {
             console.log(data);
-            section.innerHTML += `<article>
-                    <a href="./detail-movie.html?p=${idmovie}"> <img class="imagenes" src="https://image.tmdb.org/t/p/w500/${popular.poster_path}" alt=""></a>
-                    <a href="./detail-movie.html?p=${idmovie}"><h3>${popular.title}</h3></a>
-                    <a href="./detail-movie.html?p=${idmovie}"><p>${popular.release_date}</p></a>
+            let peli_f = data[i];
+            console.log(peli_f.release_date);
+            Peliculas_Favoritas.innerHTML = `<article>
+                    <a href="./detail-movie.html?p=${idmovie}"> <img class="imagenes" src="https://image.tmdb.org/t/p/w500/${peli_f.poster_path}" alt=""></a>
+                    <a href="./detail-movie.html?p=${idmovie}"><h3>${peli_f.title}</h3></a>
+                    <a href="./detail-movie.html?p=${idmovie}"><p>${peli_f.release_date}</p></a>
                     <a href="./detail-movie.html?p=${idmovie}" class="ver_mas">Ver mas</a>
                 </article>` 
-            
-
+            section += Peliculas_Favoritas;
             return data;
-        }).catch(function (error) {
+        })
+        .catch(function (error) {
             return error;
         });
 
