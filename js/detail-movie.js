53b9a99abebe3d16c05d8b6810da00d20efb82df
isayukelson <iyukelson@udesa.edu.ar>
2022-11-22 00:06:29 -0300

    terminado detalles

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,125 +1,99 @@
-//BOTÓN PARA FAVORITOS
-let qs = location.search;
-let qsObj = new URLSearchParams(qs);
-let idmovie = qsObj.get ('idmovie');
-let id = idmovie;
-let boton = document.querySelector ('.boton-fav');
+let algo = new URLSearchParams(this.location.search);
+let id_pelicula = algo.get("p");
+console.log(id_pelicula);
+
+let boton = document.querySelector(".boton-fav");
 let favoritos_p = [];
-let recuperoStorage = localStorage.getItem('favoritos_p');
+let recuperoStorage = localStorage.getItem("favoritos_p");
 
 /* Preguntamos si es distinto de nulo-  es verdarero quiero covertirlo de JSON a un array */
-if(recuperoStorage != null){
-    favoritos_p = JSON.parse(recuperoStorage);
-
-};
-if (favoritos_p.includes(idmovie)) {
-    boton.innerText="Quitar de Favorito";
+if (recuperoStorage != null) {
+  favoritos_p = JSON.parse(recuperoStorage);
 }
-boton.addEventListener("click",function (e) {
-    e.preventDefault()
-    if(favoritos_p.includes(id)){
-        let indice = favoritos_p.indexOf(id);
-        favoritos_p.splice(indice,1);
-        boton.innerText="❤ Agregar a Favorito";
-    }else{
-        favoritos_p.push(id);
-        boton.innerText="Quitar de Favorito";
-    }
-    let favToString = JSON.stringify(favoritos_p);
-    localStorage.setItem('favoritos_p',favToString)
-
+if (favoritos_p.includes(id_pelicula)) {
+  boton.innerText = "Quitar de Favorito";
+}
+boton.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (favoritos_p.includes(id)) {
+    let indice = favoritos_p.indexOf(id);
+    favoritos_p.splice(indice, 1);
+    boton.innerText = "❤ Agregar a Favorito";
+  } else {
+    favoritos_p.push(id);
+    boton.innerText = "Quitar de Favorito";
+  }
+  let favToString = JSON.stringify(favoritos_p);
+  localStorage.setItem("favoritos_p", favToString);
 });
 
-// detalle de peliculas: 
 
-let algo = new URLSearchParams(this.location.search) 
-let id_pelicula = algo.get("p")
-console.log(id_pelicula)
+const api_key = "81faef6942a31915ed87b416fbba64ba";
+let url = `https://api.themoviedb.org/3/movie/${id_pelicula}?api_key=${api_key}&language=en-US`;
 
-const api_key = '81faef6942a31915ed87b416fbba64ba'
-let url = `https://api.themoviedb.org/3/movie/${id_pelicula}?api_key=${api_key}&language=en-US`
+let subtitulos = document.querySelector(".subtitulos");
+console.log(subtitulos);
+let fecha_estreno = document.querySelector(".fecha-estreno");
+console.log(fecha_estreno);
+let genero_pelicula = document.querySelector(".genero_pelicula");
+console.log(genero_pelicula);
+let descripcion_peli = document.querySelector(".descripcion_peli");
+console.log(descripcion_peli);
+let imagen = document.querySelector("#img");
+console.log(imagen);
+let votos = document.querySelector(".votos");
+console.log(votos);
+let time = document.querySelector(".time");
+console.log(time);
 
- let subtitulos = document.querySelector('.subtitulos');
- let fecha_estreno = document.querySelector('.fecha-estreno');
- let genero_pelicula = document.querySelector('.genero_pelicula');
- let descripcion_peli = document.querySelector('.descripcion_peli');
- let imagen = document.querySelector('#img');
- let votos = document.querySelector('.votos');
- let time = document.querySelector('.time');
+fetch(url)
+  .then(function (respuesta) {
+    return respuesta.json();
+  })
+  .then(function (data) {
+    console.log(data);
 
+    subtitulos.innerText += `${data.title}`
+    fecha_estreno.innerText +=  `${data.release_date}`
+    genero_pelicula.innerText += `${data.genres[0].name}`
+    descripcion_peli.innerText += `${data.overview}`
+    imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
+    votos.innerText += `${data.vote_average}`
+    time.innerText += `${data.runtime}`
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
- fetch(url)
-     .then(function (respuesta) {
-         return respuesta.json();
-     })
-     .then(function (data) {
-         console.log(data);
-        
+// api de recomendaciones pelis:
 
-         subtitulos.innerText = data.title;
-         fecha_estreno.innerText = data.release_date;
-         genero_pelicula.innerText = data.genres[0].name;
-         descripcion_peli.innerText = data.overview;
-         imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`
-         votos.innerText= data.vote_average
-         time.innerText = data.runtime;
-     })
-     .catch(function (error) {
-                console.log(error);
-    })
-
- //boton ver  pelis similares  -- chequear codigo
-
- let botonSimil = document.getElementById(".botonSimil");
-window.onclick = function(event) {
-     if (event.target == botonSimil) {
-         botonSimil.style.display = "none";
-     }
- }
-
-
- 
- // ver titulos  similares -- chequear codigo
-
-let urlreco = `https://api.themoviedb.org/3/movie/${id_pelicula}/recommendations?api_key=81faef6942a31915ed87b416fbba64ba`
-let btn = document.querySelector('.botonSimil');
-
-
-
-//let url = `https://api.themoviedb.org/3/movie/${id_pelicula}?api_key=${api_key}&language=en-US`
-let detalle_pelicula = document.querySelector(".cont-detail")
+let urlreco = `https://api.themoviedb.org/3/movie/${id_pelicula}/recommendations?api_key=${api_key}`;
 
 fetch(urlreco)
     .then(function (respuesta) {
         return respuesta.json();
     })
     .then(function (data) {
-       detalle_pelicula.innerText +=
-       `<section class="Recomendaciones "> </section>`
-       let Recomendaciones = document.querySelector(".Recomendaciones")
-        for (let i = 0; i < 5; i++){
-            Recomendaciones.innerHTML +=
-            `<img id="img" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" ></img>`
-        }
+        let recomendaciones = document.querySelector(".recomendaciones");
         
-        console.log(data);
-        let botonSimil = document.querySelector(".Show Recomendations")
-        botonSimil.addEventListener("click", function (){
-            let Recomendaciones = document.querySelector(".Recomendaciones")
-            console.log(Recomendaciones.style.display);
-            if (Recomendaciones.style.display == "none"){
-                Recomendaciones.style.display = "block"
-                botonSimil.innerText = "ocultar"
-            } else {
-                Recomendaciones.style.display = "none"
-                botonSimil.innerText = "mostrar"
-            }
-        })
+        for (let i = 0; i < 5; i++) {
+            recomendaciones.innerHTML += `<li>
+                <a href="detail-movie.html?p=${data.results[i].id}">
+                    <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt="">
+                    <p>${data.results[i].title}</p>
+                    <p>${data.results[i].release_date}</p>
 
+                </a>
+            </li>`;
+        }
+
+        let btn_recomendaciones = document.querySelector(".btn-recomendaciones");
+
+        btn_recomendaciones.addEventListener("click", function (e) {
+            e.preventDefault();
+            recomendaciones.classList.toggle("ocultar");
+        })
     })
     .catch(function (error) {
         console.log(error);
-        return error;
-    })
-
-console.log(favoritos_p)
+    });
--- js/detail-series.js
+++ js/detail-series.js
@@ -38,9 +38,9 @@ let algo = new URLSearchParams(this.location.search)
 let tv_id = algo.get("s")
 console.log(tv_id)
 
-const api_key = '81faef6942a31915ed87b416fbba64ba'
-let Url = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}&language=en-US&page=1`
-
+const api_key = '81faef6942a31915ed87b416fbba64ba';
+let Url = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}//&language=en-US//&page=1`;
+console.log(Url)
 
 let subtitulos = document.querySelector('.subtitulos');
 let fecha_estreno = document.querySelector('.fecha-estreno');
@@ -51,9 +51,9 @@ let vote = document.querySelector('.vote');
 let time = document.querySelector('.time');
 
 
-//let url = `https://api.themoviedb.org/3/tv/top_rated${tv_id}?api_key=81faef6942a31915ed87b416fbba64ba&language=en-US&page=1`
-let url = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}&language=en-US&page=1`
 
+let url = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}&language=en-US&page=1`
+console.log(url)
 
 fetch(url)
     .then(function (respuesta) {
@@ -61,24 +61,31 @@ fetch(url)
     })
     .then(function (data) {
 
+        console.log(data);
 
          let subtitulos = document.querySelector('.subtitulos');
+         console.log(subtitulos);
          let fecha_estreno = document.querySelector('.fecha-estreno');
+         console.log(fecha_estreno)
          let genero_series = document.querySelector('.genero_serie');
+        console.log(genero_series)
          let descripcion_serie = document.querySelector('.descripcion_serie');
+            console.log(descripcion_serie)
          let imagen = document.querySelector('#img');
+            console.log(imagen)
          let vote = document.querySelector('.vote');
-         let time = document.querySelector('.time');
+            console.log(vote)
+        
 
 
         
-        subtitulos.innerText = `${ data.name }`
-        fecha_estreno.innerText = `${ data.first_air_date } `
-        genero_series.innerText = `${ data.genres[0].name } `
-        descripcion_serie.innerText = `${ data.overview } `
+        subtitulos.innerText += ` ${ data.name }`
+        fecha_estreno.innerText += ` ${ data.first_air_date } `
+        genero_series.innerText += ` ${ data.genres.name } `  
+        genero_series.innerText += ` ${ data.genres[0].name } `
+        descripcion_serie.innerText += ` ${ data.overview } `
         imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`
-        vote.innerText = `${data.vote_average}`
-        time.innerText = `${data.runtime}`
+        vote.innerText += ` ${data.vote_average}`
 
 
     })
@@ -88,15 +95,40 @@ fetch(url)
 
    
    
-    // boton de ver series similares 
+    // api de ver series recomendadas:  
    
-    let botonSimil = document.getElementById(".botonSimil");
-    window.onclick = function(event) {
-         if (event.target == botonSimil) {
-             botonSimil.style.display = "none";
-         }
-     }
+     let urlreco = `https://api.themoviedb.org/3/tv/${tv_id}/recommendations?api_key=81faef6942a31915ed87b416fbba64ba`;
+     fetch(urlreco)
+         .then(function (respuesta) {
+             return respuesta.json();
+         })
+         .then(function (data) {
+             let recomendaciones = document.querySelector(".recomendaciones");
+             console.log(recomendaciones)
+             
+             for (let i = 0; i < 5; i++) {
+                 recomendaciones.innerHTML += `<li>
+                     <a href="detail-serie.html?p=${data.results[i].id}">
+                         <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt="">
+                         <p>${data.results[i].name}</p>
+                         <p>${data.results[i].first_air_date}</p>
 
-
+                     </a>
+                 </li>`;
+             }
+     
+             let btn_recomendaciones = document.querySelector(".btn-recomendaciones");
+     
+             btn_recomendaciones.addEventListener("click", function (e) {
+                 e.preventDefault();
+                 recomendaciones.classList.toggle("ocultar");
+             })
+         })
+         .catch(function (error) {
+             console.log(error);
+         });
+     
+     
+     
 
      
